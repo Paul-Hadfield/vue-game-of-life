157a1a6ed68a6e3a19d58210c85fc51f475a02d5
Paul-Hadfield <732223+Paul-Hadfield@users.noreply.github.com>
2025-11-28 22:17:23 +0000

    Updated versions

--- gameengine.js
+++ gameengine.js
@@ -1,34 +1,25 @@
-import GridSetup from "./gridsetup";
+import GridSetup from './gridsetup';
 
 const applyRules = (isLive, liveNeighbours) => {
   if (isLive) {
-    return (liveNeighbours == 2) | (liveNeighbours == 3);
-  } else {
-    return liveNeighbours == 3;
+    return (liveNeighbours === 2) || (liveNeighbours === 3);
   }
+  return liveNeighbours === 3;
 };
 
-const excludeSelf = (cell, currentCell) => {
-  return !(cell.x == currentCell.x && cell.y == currentCell.y);
-};
+const excludeSelf = (cell, currentCell) => !(cell.x === currentCell.x && cell.y === currentCell.y);
 
-const isLive = cell => {
-  return cell.live;
-};
+const isLive = cell => cell.live;
 
-const includeNeighbours = (cell, currentCell) => {
-  return (
-    (cell.x >= currentCell.x - 1) & (cell.x <= currentCell.x + 1) &&
-    (cell.y >= currentCell.y - 1) & (cell.y <= currentCell.y + 1)
-  );
-};
+const includeNeighbours = (cell, currentCell) => (
+  (cell.x >= currentCell.x - 1) && (cell.x <= currentCell.x + 1)
+    && (cell.y >= currentCell.y - 1) && (cell.y <= currentCell.y + 1)
+);
 
-const getNumberOfLiveNeighbours = (cell, currentGrid) => {
-  return currentGrid
-    .filter(nc => excludeSelf(nc, cell))
-    .filter(nc => includeNeighbours(nc, cell))
-    .filter(isLive).length;
-};
+const getNumberOfLiveNeighbours = (cell, currentGrid) => currentGrid
+  .filter(nc => excludeSelf(nc, cell))
+  .filter(nc => includeNeighbours(nc, cell))
+  .filter(isLive).length;
 
 const GameEngine = {
   setupBlinker: () => GridSetup.blinker(),
@@ -39,13 +30,11 @@ const GameEngine = {
   setupAcorn: () => GridSetup.acorn(),
   setupDiehard: () => GridSetup.diehard(),
 
-  determineNewState: (cell, currentGrid) => {
-    return {
-      x: cell.x,
-      y: cell.y,
-      live: applyRules(cell.live, getNumberOfLiveNeighbours(cell, currentGrid))
-    };
-  }
+  determineNewState: (cell, currentGrid) => ({
+    x: cell.x,
+    y: cell.y,
+    live: applyRules(cell.live, getNumberOfLiveNeighbours(cell, currentGrid)),
+  }),
 };
 
 export default GameEngine;
--- gridsetup.js
+++ gridsetup.js
@@ -1,164 +1,164 @@
 function buildEmptyGrid(cols, rows) {
+  let x = 1;
+  let y = 1;
 
-    let x = 1;
-    let y = 1;
+  const numberOfCells = rows * cols;
 
-    const numberOfCells = rows * cols;
+  const grid = [];
 
-    let grid = [];
+  const Util = {
+    range: (min, max) => Array.from({ length: max - min + 1 }, (_, i) => min + i),
+  };
 
-    Util.range(1, numberOfCells).map(() => {
-        
-        grid.push({x, y, live: false});
-        
-        x++; 
+  Util.range(1, numberOfCells).forEach(() => {
+    grid.push({ x, y, live: false });
 
-        if( x > cols) { 
-            x = 1; y++; 
-        }
-    });
+    // eslint-disable-next-line no-plusplus
+    x++;
 
-    return grid;
-  }
+    if (x > cols) {
+      // eslint-disable-next-line no-plusplus
+      x = 1; y++;
+    }
+  });
+
+  return grid;
+}
 
 const getRandomIntInclusive = (min, max) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+  const minFloored = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(
+    Math.random(minFloored)
+    * (maxFloored - minFloored + 1),
+  ) + minFloored; // The maximum is inclusive and the minimum is inclusive
 };
 
-const Util = {
-    range: (min, max) => Array.from({length: max - min + 1}, (_, i) => min + i)
-}
-
 const GridSetup = {
-    blinker : () => {
+  blinker: () => {
+    const grid = buildEmptyGrid(5, 5);
 
-        let grid = buildEmptyGrid(5, 5);
-        
-        grid
-        .filter(c => c.x >= 2 && c.x <= 4)
-        .filter(c => c.y == 3)
-        .forEach(cell => { cell.live = true; });
+    grid
+      .filter(c => c.x >= 2 && c.x <= 4)
+      .filter(c => c.y === 3)
+      .forEach((cell) => { cell.live = true; });
 
-        return grid;
-    },
-    toad : () => {
-        
-        let grid = buildEmptyGrid(6, 6);
-    
-        grid
-          .filter(c => c.x >= 2 && c.x <= 4)
-          .filter(c => c.y == 3)
-          .forEach(cell => { cell.live = true; });
-        grid
-          .filter(c => c.x >= 3 && c.x <= 5)
-          .filter(c => c.y == 4)
-          .forEach(cell => { cell.live = true; });
+    return grid;
+  },
+  toad: () => {
+    const grid = buildEmptyGrid(6, 6);
 
-        return grid;
-      }, 
-    beacon : () => {
-        
-        let grid = buildEmptyGrid(6, 6);
-    
-        grid
-          .filter(c => c.x == 2 || c.x == 3)
-          .filter(c => c.y == 2 || c.y == 3)
-          .forEach(cell => { cell.live = true; });
-        grid
-          .filter(c => c.x == 4 || c.x == 5)
-          .filter(c => c.y == 4 || c.y == 5)
-          .forEach(cell => { cell.live = true; });
+    grid
+      .filter(c => c.x >= 2 && c.x <= 4)
+      .filter(c => c.y === 3)
+      .forEach((cell) => { cell.live = true; });
+    grid
+      .filter(c => c.x >= 3 && c.x <= 5)
+      .filter(c => c.y === 4)
+      .forEach((cell) => { cell.live = true; });
 
-        return grid;
-      },
-    pulsar : () => {
-        let grid = buildEmptyGrid(17, 17);
+    return grid;
+  },
+  beacon: () => {
+    const grid = buildEmptyGrid(6, 6);
 
-        grid
-          .filter(c => ((c.x >= 5 && c.x <= 7) || (c.x >= 11 && c.x <= 13)) && (c.y == 3))
-          .forEach(cell => { cell.live = true; });
+    grid
+      .filter(c => c.x === 2 || c.x === 3)
+      .filter(c => c.y === 2 || c.y === 3)
+      .forEach((cell) => { cell.live = true; });
+    grid
+      .filter(c => c.x === 4 || c.x === 5)
+      .filter(c => c.y === 4 || c.y === 5)
+      .forEach((cell) => { cell.live = true; });
 
-        grid
-          .filter(c => ((c.x >= 5 && c.x <= 7) || (c.x >= 11 && c.x <= 13)) && (c.y == 8))
-          .forEach(cell => { cell.live = true; });
+    return grid;
+  },
+  pulsar: () => {
+    const grid = buildEmptyGrid(17, 17);
 
-        grid
-          .filter(c => ((c.x >= 5 && c.x <= 7) || (c.x >= 11 && c.x <= 13)) && (c.y == 10))
-          .forEach(cell => { cell.live = true; });
+    grid
+      .filter(c => ((c.x >= 5 && c.x <= 7) || (c.x >= 11 && c.x <= 13)) && (c.y == 3))
+      .forEach((cell) => { cell.live = true; });
 
-        grid
-          .filter(c => ((c.x >= 5 && c.x <= 7) || (c.x >= 11 && c.x <= 13)) && (c.y == 15))
-          .forEach(cell => { cell.live = true; });
+    grid
+      .filter(c => ((c.x >= 5 && c.x <= 7) || (c.x >= 11 && c.x <= 13)) && (c.y == 8))
+      .forEach((cell) => { cell.live = true; });
 
-        grid
-          .filter(c => ((c.y >= 5 && c.y <= 7) || (c.y >= 11 && c.y <= 13)) && (c.x == 3))
-          .forEach(cell => { cell.live = true; });
+    grid
+      .filter(c => ((c.x >= 5 && c.x <= 7) || (c.x >= 11 && c.x <= 13)) && (c.y == 10))
+      .forEach((cell) => { cell.live = true; });
 
-        grid
-          .filter(c => ((c.y >= 5 && c.y <= 7) || (c.y >= 11 && c.y <= 13)) && (c.x == 8))
-          .forEach(cell => { cell.live = true; });
+    grid
+      .filter(c => ((c.x >= 5 && c.x <= 7) || (c.x >= 11 && c.x <= 13)) && (c.y == 15))
+      .forEach((cell) => { cell.live = true; });
 
-        grid
-          .filter(c => ((c.y >= 5 && c.y <= 7) || (c.y >= 11 && c.y <= 13)) && (c.x == 10))
-          .forEach(cell => { cell.live = true; });
+    grid
+      .filter(c => ((c.y >= 5 && c.y <= 7) || (c.y >= 11 && c.y <= 13)) && (c.x == 3))
+      .forEach((cell) => { cell.live = true; });
 
-        grid
-          .filter(c => ((c.y >= 5 && c.y <= 7) || (c.y >= 11 && c.y <= 13)) && (c.x == 15))
-          .forEach(cell => { cell.live = true; });
+    grid
+      .filter(c => ((c.y >= 5 && c.y <= 7) || (c.y >= 11 && c.y <= 13)) && (c.x == 8))
+      .forEach((cell) => { cell.live = true; });
 
-        return grid;
-      },
-    random : () => {
-        let grid = buildEmptyGrid(50, 50);
-        
-        grid.forEach(cell => { cell.live = getRandomIntInclusive(0, 100) > 66; });
-    
-        return grid;
-      },
-      acorn : () => {
-        let grid = buildEmptyGrid(9, 5);
+    grid
+      .filter(c => ((c.y >= 5 && c.y <= 7) || (c.y >= 11 && c.y <= 13)) && (c.x == 10))
+      .forEach((cell) => { cell.live = true; });
 
-        grid
-            .filter(c => (c.x >= 2 && c.x <= 3) && c.y == 4)
-            .forEach(cell => { cell.live = true; });
+    grid
+      .filter(c => ((c.y >= 5 && c.y <= 7) || (c.y >= 11 && c.y <= 13)) && (c.x == 15))
+      .forEach((cell) => { cell.live = true; });
 
-        grid
-            .filter(c => (c.x >= 6 && c.x <= 8) && c.y == 4)
-            .forEach(cell => { cell.live = true; });
+    return grid;
+  },
+  random: () => {
+    const grid = buildEmptyGrid(50, 50);
 
-        grid
-            .filter(c => c.x == 3 && c.y == 2)
-            .forEach(cell => { cell.live = true; });
+    grid.forEach((cell) => { cell.live = getRandomIntInclusive(0, 100) > 66; });
 
-        grid
-            .filter(c => c.x == 5 && c.y == 3)
-            .forEach(cell => { cell.live = true; });
+    return grid;
+  },
+  acorn: () => {
+    const grid = buildEmptyGrid(9, 5);
 
-        return grid;
-      },
-    diehard : () => {
-        let grid = buildEmptyGrid(10, 5);
+    grid
+      .filter(c => (c.x >= 2 && c.x <= 3) && c.y === 4)
+      .forEach((cell) => { cell.live = true; });
 
-        grid
-            .filter(c => (c.x >= 7 && c.x <= 9) && c.y == 4)
-            .forEach(cell => { cell.live = true; });
+    grid
+      .filter(c => (c.x >= 6 && c.x <= 8) && c.y === 4)
+      .forEach((cell) => { cell.live = true; });
 
-        grid
-            .filter(c => c.x == 8 && c.y == 2)
-            .forEach(cell => { cell.live = true; });
-        
-        grid
-            .filter(c => (c.x >= 2 && c.x <= 3) && c.y == 3)
-            .forEach(cell => { cell.live = true; });
-        
-        grid
-            .filter(c => c.x == 3 && c.y == 4)
-            .forEach(cell => { cell.live = true; });
+    grid
+      .filter(c => c.x === 3 && c.y === 2)
+      .forEach((cell) => { cell.live = true; });
 
-        return grid;
-      }
-}
+    grid
+      .filter(c => c.x === 5 && c.y === 3)
+      .forEach((cell) => { cell.live = true; });
+
+    return grid;
+  },
+  diehard: () => {
+    const grid = buildEmptyGrid(10, 5);
+
+    grid
+      .filter(c => (c.x >= 7 && c.x <= 9) && c.y === 4)
+      .forEach((cell) => { cell.live = true; });
+
+    grid
+      .filter(c => c.x === 8 && c.y === 2)
+      .forEach((cell) => { cell.live = true; });
+
+    grid
+      .filter(c => (c.x >= 2 && c.x <= 3) && c.y == 3)
+      .forEach((cell) => { cell.live = true; });
+
+    grid
+      .filter(c => c.x === 3 && c.y === 4)
+      .forEach((cell) => { cell.live = true; });
+
+    return grid;
+  },
+};
 
 export default GridSetup;
